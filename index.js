b351bc8c2cd94d7b716c12288f514b6e355b52ec
Davinson vanegas <dvanegas@cib.org.co>
2024-10-01 16:56:14 -0500

    Se modifica los archivos con los nuevos cambiios realizados al estilo como en la iteracion con js, proyecto a un 80% de completada

--- index.js
+++ index.js
@@ -1,106 +1,250 @@
-const carousel = document.querySelector('.CIB_miniaturas');
-let isDragging = false;
-let startX;
-let scrollLeft;
+// datos de las imagenes
+const imagenes = {
+  // 0: {
+  //   img: "https://youtu.be/B29M9JG8Q9o",
+  //   title: "Nuestra Historia",
+  //   description:
+  //     "CIB - Corporación para investigaciones biológicas",
+  //   date: "1970",
+  // },
+  1: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/1970.jpg",
+    title: "1970",
+    description:
+      "La Corporación para Investigaciones Biológicas nace en 1970, gracias 3 hechos puntuales: la firma del acta de constitución y aprobación de los estatutos; la realización del Acta de Fundación, y el desarrollo del primer proyecto de investigación. Nuestro origen fue posible gracias al apoyo y acompañamiento de la Universidad de Antioquia y su Facultad de Medicina.",
+    date: "1970",
+  },
+  2: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/1974-1975.jpg",
+    title: "1974-1975",
+    description:
+      "Se realiza la segunda edición del libro Fundamentos de Medicina, que pasa a estar bajo propiedad de la CIB.",
+    date: "1974-1975",
+  },
+  3: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img//1976.jpg",
+    title: "1976",
+    description:
+      "Entra en funcionamiento nuestra sede en el Parque de Berrío, y nos dedicamos por completo a la publicación de los libros de medicina interna.",
+    date: "1976",
+  },
+  4: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img//1978.jpg",
+    title: "1978",
+    description:
+      "Gracias a la asociación con el Hospital Pablo Tobón Uribe, nos constituimos como centro de investigación, con un laboratorio cuyos equipos fueron donados por la Dra. Charlotte Campbell, catedrática de Harvard. Es mismo año inauguramos el servicio de diagnóstico especializado en micología.",
+    date: "1978",
+  },
+  5: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/1979-1980.jpg",
+    title: "1979-1980",
+    description:
+      "Damos inicio a las secciones de inmunología, infectología pediátrica y parasitología. Posteriormente, la unidad de infectología pediátrica, creada por el Dr. Hugo Trujillo Soto, se convertiría en lo que hoy es la unidad de bacteriología y micobacterias.",
+    date: "1979-1980",
+  },
+  6: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/1980.jpg",
+    title: "1980",
+    description:
+      "Nace el club de revista del Dr. Rojas y se da la vinculación de médicos y bacteriólogos en su año rural.",
+    date: "1980",
+  },
+  7: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/1983.jpeg",
+    title: "1983",
+    description:
+      "Se realiza la primera edición del Manual de Terapéutica, uno de los textos médicos más exitosos en Latinoamérica.",
+    date: "1983",
+  },
+  8: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/1985.jpeg",
+    title: "1985",
+    description:
+      "Gracias a las gestiones realizadas con la Organización Panamericana de la Salud, se realiza una internacionalización del Fondo Editorial con la obra Fundamentos de Medicina.",
+    date: "1985",
+  },
+  9: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/1989.jpg",
+    title: "1989",
+    description:
+      "Incursionamos en la docencia a través de la formación de jóvenes investigadores y apoyo a las entidades de salud en su objeto institucional.",
+    date: "1989",
+  },
+  10: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/1991-1992.jpg",
+    title: "1991-1992",
+    description:
+      "Una vez finalizados sus estudios, varios investigadores se unen a los laboratorios de la CIB, lo que permite la creación de la Unidad de Biología Celular y Molecular.",
+    date: "1970-09-2020",
+  },
+  11: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/1994.jpg",
+    title: "1994",
+    description:
+      "Comienza la construcción de nuestra sede propia. Ese mismo año, creamos la línea de Cardiología con énfasis en hipertensión arterial y realizamos la primera edición del libro Fundamentos de Pediatría.",
+    date: "1994",
+  },
+  12: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/1998-2001.jpg",
+    title: "1998-2001",
+    description:
+      "El trabajo conjunto con las universidades nos convierte en campo de práctica para sus estudiantes de pregrado y posgrado.",
+    date: "1998-2001",
+  },
+  13: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/2003.jpg",
+    title: "2003",
+    description:
+      "Creamos oficialmente la Unidad de Biodiversidad.",
+    date: "2003",
+  },
+  14: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/2004.jpg",
+    title: "2004",
+    description:
+      "El Centro de Excelencia en Tuberculosis es establecido por Colciencias y firmamos más acuerdos de cooperación con instituciones nacionales e internacionales.",
+    date: "2004",
+  },
+  15: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/2006.jpg",
+    title: "2006",
+    description:
+      "Junto con la Universidad de Antioquia y la Universidad Nacional de Colombia, logramos nuestra primera patente.",
+    date: "2006",
+  },
+  16: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/2009.jpg",
+    title: "2009",
+    description:
+      "Recibimos la visita del Premio Nobel de Medicina y Fisiología de 1993, Sir Richard Roberts. Ese mismo años obtenemos los certificados de ICONTEC ISO 9001 e IQNet.",
+    date: "2009",
+  },
+  17: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/2010FOTO1.jpg",
+    title: "2010",
+    description:
+      "Nuevos PHd se vinculan a nuestras distintas unidades de investigación. En convenio con la Universidad del Rosario, abrimos la línea de investigación de Bioinformática y abrimos el Centro Clínico y de Investigación SICOR.",
+    date: "2010",
+  },
+  18: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/2010FOTO2.jpg",
+    title: "2010",
+    description:
+      "Este mismo año nuestra Corporación fue considerada como el mejor centro de investigación del país, en el estudio Caracterización de Centros Autónomos de Investigación y Desarrollo Tecnológico, desarrollado por el Observatorio Colombiano de Ciencia y Tecnología.",
+    date: "2010",
+  },
+  19: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/2017.png",
+    title: "2017",
+    description:
+      "La Universidad de Santander se vincula como socio de la CIB.",
+    date: "2017",
+  },
+  20: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/2018.jpg",
+    title: "2018",
+    description:
+      "La CIB es reconocida como centro de investigación por cinco años más. Esta distinción nos consolida como una institución que aporta a la generación de conocimiento en nuestro país. El reconocimiento lo otorga la Política Nacional de Actores del Sistema Nacional de Ciencia, Tecnología e Innovación (SNCTeI).",
+    date: "2018",
+  },
+  21: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/2019.jpeg",
+    title: "Orden al Mérito Don Juan del Corral, grado Plata.",
+    description:
+      "Ceremonia de entrega de la distinción Orden al Mérito Don Juan del Corral, grado Plata, a la Corporación para Investigaciones Biológicas por sus 49 años de promover investigación sostenible al servicio de la vida. Gracias al Concejo de Medellín y al concejal Ricardo León Yepez Pérez por este gran reconocimiento.",
+    date: "2019",
+  },
+  22: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/2020.png",
+    title: "2020",
+    description:
+      "Mediante Acuerdo municipal N°39 de 1998, el Concejo de Medellín creó la condecoración Orquídea Concejo de Medellín, máximo reconocimiento entregado por la Corporación a personas, entidades o instituciones nacionales o extranjeras que en desarrollo de sus actividades agroindustriales, científicas, tecnológicas, deportivas, ecológicas y cívicas, educativas y culturales, empresariales y humanas, así como de defensa de los animales, se destaquen en el ámbito regional, nacional y/o internacional, como aporte a la humanidad, a nuestra cultura y a nuestra ciudad.",
+    date: "2020",
+  },
+  23: {
+    img: "https://sistemascib.github.io/Nuestra_historia/img/2021.png",
+    title: "2021",
+    description:
+      "Colombiano Ejemplar 2021. La CIB Medellín recibió el reconocimiento al Colombiano ejemplar 2021 en la categoría Ciencia y Tecnología. Seguimos trabajando arduamente para fortalecer nuestro propósito superior y contribuir al desarrollo de nuestra región: “Promover investigación sostenible al servicio de la vida”. #lacienciaalserviciodelavida#ElColombianoEjemplar2021",
+    date: "2021",
+  },
+};
 
-carousel.addEventListener('mousedown', (e) => {
-  isDragging = true;
-  carousel.classList.add('active');
-  startX = e.pageX + carousel.offsetLeft;
-  scrollLeft = carousel.scrollLeft;
-  carousel.style.cursor = 'grabbing'; // Cambia el cursor al arrastrar
-});
+// Datos a recuperar del html
+const foto = document.querySelector(".foto_historia");
+const title = document.querySelector(".title");
+const description = document.querySelector(".description");
+const date = document.querySelector(".date");
+const containerImages = document.querySelector(".CIB_miniaturas");
 
-carousel.addEventListener('mouseleave', () => {
-  isDragging = false;
-  carousel.classList.remove('active');
-  carousel.style.cursor = 'grab'; // Restablece el cursor cuando se sale del carrusel
-});
+// esta funcion recibe el indice de la foto para cambiar la informacion
+function cambiarDate(indice) {
+  foto.setAttribute("src", imagenes[indice]["img"]);
+  foto.setAttribute("id", indice);
 
-carousel.addEventListener('mouseup', () => {
-  isDragging = false;
-  carousel.classList.remove('active');
-  carousel.style.cursor = 'grab'; // Restablece el cursor al soltar
-});
+  description.textContent = imagenes[indice]["description"];
+  title.textContent = imagenes[indice]["title"];
+  date.textContent = imagenes[indice]["date"];
+}
 
-carousel.addEventListener('mousemove', (e) => {
-  if (!isDragging) return; // Detiene la función si no se está arrastrando
+window.indice; //variable global en el codigo
+
+// esta funcion recibe lado y indice, cambia la informacion dependiendo la flecha pulsada
+
+function pasar(lado, indice) {
+  let num = parseInt(indice);
+
+  if ((num === 0 && lado === "izq") || (num === 1 && lado === "izq"))
+    indice = 1;
+  if (num > 1 && lado === "izq") indice--;
+
+  if (num >= 1 && lado === "der" && num < Object.values(imagenes).length)
+    indice++;
+  if (num === 0 && lado === "der") indice = 2;
+
+  cambiarDate(indice);
+}
+
+// recupera el evento click en la flecha izquierda
+document.querySelector(".izq").addEventListener("click", (e) => {
   e.preventDefault();
-  const x = e.pageX + carousel.offsetLeft;
-  const walk = (x - startX) * 2; // Ajusta la velocidad del desplazamiento
-  carousel.scrollLeft = scrollLeft - walk;
+  pasar("izq", foto.id);
 });
 
+// recupera el evento click en la flecha derecha
+document.querySelector(".der").addEventListener("click", (e) => {
+  e.preventDefault();
+  pasar("der", foto.id);
+});
 
+window.contador = 1; // contador global para los indices
 
+// iteratua entre todas las imagenes y crea la miniatura de las mismas
+Object.values(imagenes).forEach((item) => {
+  let nodo = `
+      <img 
+      class="miniatura"
+      src="${item["img"]}" 
+      alt="${item["title"]}" 
+      title="${item["title"]}"
+      width="100"
+      height="100"
+      loading="lazy"
+      id="${contador}"
+      >
+  `;
 
+  containerImages.innerHTML += nodo; // lo incrustamos en el html
+  contador++; // sumamos 1 para el id de las imagenes
+});
 
-
-
-
-let fotoCambiar = document.querySelector(".foto_historia");
-
-window.val;
-
-let n = {
-    1 : "https://sebastian-alvarez-diaz.vercel.app/img/maquina.jpg",
-    2 : "https://portafolio-dv.netlify.app/img/mia.jpg",
-    3 : "https://sebastian-alvarez-diaz.vercel.app/img/maquina.jpg",
-    4 : "https://portafolio-dv.netlify.app/img/mia.jpg",
-    5 : "https://sebastian-alvarez-diaz.vercel.app/img/maquina.jpg",
-};
-
-
-function pasar(lado ,indice) {
-
-    let num = parseInt(indice)
-
-    if((num === 0 && lado === "izq") || (num === 1 && lado === "izq")) val = 1;
-    if (num > 1 && lado === "izq" ) val--
-
-
-    if (num >= 1 && lado === "der" && num < Object.values(n).length) val++
-    if(num === 0 && lado === "der") val = 2;
-
-
-    fotoCambiar.setAttribute("src", n[val]);
-    fotoCambiar.id = val;
-
-};
-
-
-
-document.querySelector(".izq").addEventListener("click" , (e) => {
+// añade el envento click en cada miniatura de las imagenes
+document.querySelectorAll(".miniatura").forEach((item) => {
+  item.addEventListener("click", (e) => {
     e.preventDefault();
 
-    pasar("izq" , fotoCambiar.id);
-
-
+    cambiarDate(item.getAttribute("id"));
+    val = item.getAttribute("id");
+  });
 });
 
-document.querySelector(".der").addEventListener("click" , (e) => {
-    e.preventDefault();
-
-    pasar("der" , fotoCambiar.id);
-
-});
-
-let containerImages = document.querySelector(".CIB_miniaturas");
-
-window.ite = 1
-Object.values(n).forEach((item) => {
-
-    let nodo = `
-        <img 
-        src="${item}" 
-        alt="foto de historia" 
-        title="foto historia"
-        width="100"
-        height="100"
-        loading="lazy"
-        id="${ite}"
-        >
-    `
-    containerImages.innerHTML += nodo
-    ite++
-})
